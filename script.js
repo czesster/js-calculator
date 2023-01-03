6edd624ac7eb2af6e274df07f42e60826c38f929
czesster <102236299+czesster@users.noreply.github.com>
2023-01-03 18:01:47 +0100

    add script.js, selected all buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const clearBtn = document.querySelector("[data-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const equalsBtn = document.querySelector("[data-equals]");
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
